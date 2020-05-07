d825ddfdce4bd2a3ce2371ecb21d3e11431f5241
Emmanuel Adu <aduemmanuelk21@gmail.com>
2020-05-07 05:20:32 -0400

    removed emails from settings

--- payments/settings.py
+++ payments/settings.py
@@ -140,12 +140,7 @@ LOGIN_URL = 'login'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_PORT= 587
-EMAIL_HOST_USER= os.environ.get('email')
-EMAIL_HOST_PASSWORD=os.environ.get('password')
-EMAIL_USE_TLS = True
+
 
 
 try:
